f8f4b03083395e1d1fb5060ee891ac5bb0a1c531
raymond-feng26 <f.raymond@wustl.edu>
2025-11-18 12:03:58 -0600

    unitree go2

--- part23.m
+++ part23.m
@@ -5,7 +5,7 @@ M_f=[1,0,0,0;
     -1/f,1,0,0;
     0,0,1,0;
     0,0,-1/f,1]; % lens matrix
-d2=0.24% d2=0.24 is the best given f=0.15
+d2=0.24; % d2=0.24 is the best given f=0.15
 M_d2 = [1, d2, 0, 0; 0, 1, 0, 0; 0, 0, 1, d2; 0, 0, 0, 1];
 rays_out = M_d2 * M_f * rays; 
 [img, x, y] = rays2img(rays_out(1,:), rays_out(3,:), 0.003, 1000); % we use 1000 because it feels best from part2.2(1)
@@ -14,7 +14,7 @@ figure;
 image(x([1 end]), y([1 end]), img_flipped); axis image xy; % somehow the image in function is not working, do it here
 title(sprintf('f=%.2f, d2=%.2f', f, d2));
 
-% it's a bruno, a unitree robot and a WashU badge
+% it's a bruno, a unitree Go2 and a WashU badge
 % f = 0.15;
 
 %{
