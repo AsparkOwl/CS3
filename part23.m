7bdaf2f5fa312ce9359de4807ac0823bc329a2c0
raymond-feng26 <f.raymond@wustl.edu>
2025-11-19 22:41:41 -0600

    algorithm updated again, use this one

--- part23.m
+++ part23.m
@@ -1,15 +1,13 @@
 %% Part 2.2.3
 load lightField.mat
-f=0.2; % tuned f
-d2=0.4; % tuned d2, would be explained in report
+f=0.15; % tuned f
+d2=0.24; % tuned d2, would be explained in report
 r_lens=0.025;  % Lens radius
 
-lens_positions=[
-    0,0;           % Lens centered
-    -0.05,0;      % Shifted left 
-    0.05,0;       % Shifted right
-];
-
+theta_x=rays(2,:); % the three objects has different input angles, we sort the angle and devide them into 3
+theta_x_sorted=sort(theta_x);
+thresh1=theta_x_sorted(round(end/3)); % 1/3 thrshold
+thresh2=theta_x_sorted(round(2*end/3)); % 2/3 threshold
 M_f=[1,0,0,0;
     -1/f,1,0,0;
     0,0,1,0;
@@ -19,24 +17,22 @@ M_d2=[1,d2,0,0;
     0,0,1,d2;
     0,0,0,1];  % from equation 6
 
-for i=1:size(lens_positions,1)
-    x_lens=lens_positions(i,1);
-    y_lens=lens_positions(i,2);
-    
-    hit_lens=abs(rays(1,:)-x_lens)<= r_lens; % Check which rays hit the lens (relative to lens center)
-    num_rays=sum(hit_lens); % the number of rays hitting the lens. Idealy it should be 1000000, it is used to tune the shifting value 0.05 above
-    rays_subset=rays(:,hit_lens); % Only process rays that hit the lens
-    
-    % Apply optical system
-    rays_out=M_d2 * M_f * rays_subset;
-    [img,x,y]=rays2img(rays_out(1,:), rays_out(3,:),0.005,500);   
+% Three subsets based on angle threshold
+rays_set1 = rays(:,theta_x <= thresh1);
+rays_set2 = rays(:,theta_x > thresh1 & theta_x <= thresh2);
+rays_set3 = rays(:,theta_x > thresh2);
+ray_all={rays_set1,rays_set2,rays_set3}; % group them together for for loop
+
+for i=1:3 % produce 3 images
+    rays_subset=ray_all{i};
+    rays_out = M_d2 * M_f * rays_subset; % compute rays out 
+    [img, x, y] = rays2img(rays_out(1,:), rays_out(3,:), 0.003, 300);
     figure;
     img_flipped= flip(flip(img,1),2); % flip the upside down image
     image(x([1 end]),y([1 end]), img_flipped); axis image xy;
     colormap gray; % set colormap to gray
-    title(sprintf('Lens at (%.4f, %.4f), radius=%.4f, rays hitting= %d',x_lens,y_lens,r_lens,num_rays));
+    title(sprintf('Angle subset %d', i));
 end
-
 
 % it's a bruno, a unitree Go2 and a WashU badge
 % f = 0.15;
